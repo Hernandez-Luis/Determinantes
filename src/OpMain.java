2fe0c4954c9f2ef6b5b2ffb25217e9fdfd3c0e35
Hernandez-Luis <hrmz.luis@gmail.com>
2023-04-28 16:49:06 -0600

    codigoconinterface

--- src/OpMain.java
+++ src/OpMain.java
@@ -1,17 +1,60 @@
 import java.util.Scanner;
 
-public class OpMain {
+public class OpMain implements Operaciones {
 
     public void op(){
         Scanner scanner = new Scanner(System.in);
-        Llenar llenar = new Llenar();
-        Resultado resultado = new Resultado();
-
+        
         int filas =2;
         int columnas =3;
-        double mattriz[][]=new double[filas][columnas];
+        double matriz[][]=new double[filas][columnas];
 
-        mattriz = llenar.ingresa(scanner,mattriz);
-        resultado.solucion(scanner,mattriz);
+        matriz = ingresa(scanner,matriz);
+        System.out.println("Matiriz:");
+        matriz(matriz);
+        solucion(scanner,matriz);
+    }
+
+    @Override
+    public void solucion(Scanner scanner, double[][] matriz) {
+        double valor1,valor2;
+
+        System.out.println("\nResultados:");
+        valor1=(matriz[0][2]*matriz[1][1])-(matriz[1][2]*matriz[0][1]);
+        valor2=(matriz[0][0]*matriz[1][1])-(matriz[1][0]*matriz[0][1]);
+        System.out.println("X = " + (valor1/valor2));
+        valor1=(matriz[0][0]*matriz[1][2])-(matriz[1][0]*matriz[0][2]);
+        valor2=(matriz[0][0]*matriz[1][1])-(matriz[1][0]*matriz[0][1]);
+        System.out.println("Y = " + (valor1/valor2));
+    }
+
+    @Override
+    public void matriz(double[][] matriz) {
+        for (double[] doubles : matriz) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(doubles[j] + " | ");
+            }
+            System.out.println();
+        }
+    }
+
+    @Override
+    public double[][] ingresa(Scanner scanner, double[][] matriz) {
+        int cont = 0;
+        for (int i = 0; i < 2; i++) {
+            cont = 0;
+            for (int j = 0; j < 3; j++) {
+                if (i == 0) {
+                    System.out.println("Ingresa los datos" + (cont + 1) + ":");
+                    matriz[i][j] = scanner.nextDouble();
+                } else {
+                    System.out.println("ingresa y" + (cont + 1) + ":");
+                    matriz[i][j] = scanner.nextDouble();
+                }
+                cont++;
+            }
+            System.out.println();
+        }
+        return matriz;
     }
 }
---
+++ src/Operaciones.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public interface Operaciones {
+    void solucion(Scanner scanner, double [][] matriz);
+    void matriz(double [][] matriz);
+    public double [][] ingresa(Scanner scanner,double [][] matriz);
+}
